9acbef285d10f2c307a08b880eace504994546cf
Diogo <diogodcmateus@gmail.com>
2024-02-27 12:38:48 +0000

    Created new files

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+# Stores the terraform state file in S3
+  backend "s3" {
+    bucket  = "terraform-state-bucket-dm"
+    key     = "terraform.tfstate.dev"
+    region  = "us-east-1"
+    profile = "terraform-user"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region  = "us-east-1"
+  profile = "terraform-user"
+}
